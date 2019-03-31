5073f047fc8e762d5f5f2e88feae5d4e4978244f
Alexey Artemiev <fizz4ever1337@gmail.com>
2019-03-31 22:54:04 +0300

    PEP 8 AAAAa

--- KMT_method.py
+++ KMT_method.py
@@ -1,17 +1,19 @@
 def Kmt_method_test(X, Y):
-    if (isinstance(X, np.ndarray) == True) | (isinstance(X, list) == True):
+    if (isinstance(X, np.ndarray) is True) | (isinstance(X, list) is True):
         FirstP = True
 
-    if (isinstance(Y, np.ndarray) == True) | (isinstance(Y, list) == True):
+    if (isinstance(Y, np.ndarray) is True) | (isinstance(Y, list) is True):
         SecondP = True
-    # Creating 2 bool param named FirstP & SecondP what get TRUE
+        # Creating 2 bool param named FirstP & SecondP what get TRUE
     if X & Y are lists or np.ndarray types#
-    if func params type is not a valueble or another error func always return list with[0, 0]
+    if func params type is not a valueble
+    or another error func always return list with[0, 0]
 
-    if ((FirstP == False) & (SecondP == False)) | ((FirstP == False) | (SecondP == False)):
-        print("функция принимает только списки или массивы")
+    if ((FirstP is False) & (SecondP is False)) | 
+    ((FirstP is False) | (SecondP is False)):
+        print("функция прин. только списки или массивы")
         return [0, 0]
-    else :
+    else:
 
         q = 0
         w = 0
@@ -30,21 +32,21 @@ def Kmt_method_test(X, Y):
 
             try:
                 for i in range(0, N):
-                  q += N * X[i] * Y[i]
-             w += X[i]
-             e += Y[i]
-             r = q - w * e
+                    q += N * X[i] * Y[i]
+                    w += X[i]
+                    e += Y[i]
+                r = q - w * e
 
                 for i in range(0, N):
-                  t = t + N * (X[i] ** 2)
-                  u = u + X[i]
+                    t = t + N * (X[i] ** 2)
+                    u = u + X[i]
 
-             u = (u ** 2)
+                u = (u ** 2)
 
                 for i in range(0, N):
-                  a = r / (t - u)
-             b = 1 / N * (e - a * w)
-             return [a, b]
+                    a = r / (t - u)
+                    b = 1 / N * (e - a * w)
+                return [a, b]
             except:
-              print("Значения в листе должны быть числовыми")
-              return [0, 0]
+                print("Значения в листе должны быть числовыми")
+                return [0, 0]
