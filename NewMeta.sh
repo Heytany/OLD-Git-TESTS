5eab18ca5af2579b42f622e67fa49eeace541926
Alexey Artemiev <fizz4ever1337@gmail.com>
2019-04-01 18:03:42 +0300

    NO pip install sphinx cd ./Sphinxter printf 'y\ny\ny\ny\n\n\n\n' | sphinx-quickstart sphinx-build -b html sourcedir builddir make html

--- NewMeta.sh
+++ NewMeta.sh
@@ -2,8 +2,3 @@ pip install --upgrade pip
 pip install pycodestyle
 pycodestyle *.py
 
-pip install sphinx
-cd ./Sphinxter
-printf 'y\ny\ny\ny\n\n\n\n' | sphinx-quickstart
-sphinx-build -b html sourcedir builddir
-make html
